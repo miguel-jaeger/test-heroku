27fd185725e076f067e531480f52edb84d7c3ba0
miguel-jaeger <miguel.jaeger@gmail.com>
2025-10-16 11:43:20 -0500

    creado test

--- heroku/src/test/java/com/ejemplo/heroku/HerokuApplicationTests.java
+++ heroku/src/test/java/com/ejemplo/heroku/HerokuApplicationTests.java
@@ -2,6 +2,7 @@ package com.ejemplo.heroku;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -9,6 +10,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 @SpringBootTest
+@AutoConfigureMockMvc
 class HerokuApplicationTests {
 
 	
