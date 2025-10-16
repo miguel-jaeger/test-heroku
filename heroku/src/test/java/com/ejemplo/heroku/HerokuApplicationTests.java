f4dae3296e7a55c09705051fb80e2dc9d497b1bf
miguel-jaeger <miguel.jaeger@gmail.com>
2025-10-16 11:38:57 -0500

    agregado test

--- heroku/src/test/java/com/ejemplo/heroku/HerokuApplicationTests.java
+++ heroku/src/test/java/com/ejemplo/heroku/HerokuApplicationTests.java
@@ -1,13 +1,25 @@
 package com.ejemplo.heroku;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 @SpringBootTest
 class HerokuApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void statusEndpointShouldReturnOK() throws Exception {
+        mockMvc.perform(get("/status"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("OK"));
+    }
 
 }
