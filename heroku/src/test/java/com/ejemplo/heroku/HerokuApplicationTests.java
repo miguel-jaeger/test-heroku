26c87bdea6f9b034e883cacb8380dc264fda89d9
miguel-jaeger <miguel.jaeger@gmail.com>
2025-10-16 11:34:05 -0500

    agregado proyecto spring

---
+++ heroku/src/test/java/com/ejemplo/heroku/HerokuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejemplo.heroku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerokuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
