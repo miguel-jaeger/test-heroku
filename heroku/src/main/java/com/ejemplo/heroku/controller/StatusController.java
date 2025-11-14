877b8afdfc6f2dbaee5f805fa0a952a96cd18518
miguel-jaeger <84285594+miguel-jaeger@users.noreply.github.com>
2025-11-13 19:37:47 -0500

    Change status response from 'Error' to 'OK'

--- heroku/src/main/java/com/ejemplo/heroku/controller/StatusController.java
+++ heroku/src/main/java/com/ejemplo/heroku/controller/StatusController.java
@@ -9,6 +9,6 @@ public class StatusController {
 
     @GetMapping("/status")
     public ResponseEntity<String> status() {
-        return ResponseEntity.ok("Error");
+        return ResponseEntity.ok("OK");
     }
 }
