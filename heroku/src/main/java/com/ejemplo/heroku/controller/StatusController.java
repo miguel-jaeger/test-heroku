9db65ec7b57bf7268f23889a12b5e34adc72237c
miguel-jaeger <miguel.jaeger@gmail.com>
2026-06-22 09:54:45 -0500

    change message

--- heroku/src/main/java/com/ejemplo/heroku/controller/StatusController.java
+++ heroku/src/main/java/com/ejemplo/heroku/controller/StatusController.java
@@ -9,6 +9,6 @@ public class StatusController {
 
     @GetMapping("/status")
     public ResponseEntity<String> status() {
-        return ResponseEntity.ok("otra cosaaaaa");
+        return ResponseEntity.ok("Ok");
     }
 }
