31ed63ce90fa9033d102cf1ad50b6c5b59ee3eec
miguel-jaeger <84285594+miguel-jaeger@users.noreply.github.com>
2026-06-25 19:48:37 -0500

    Update status response from 'NotOk-new' to 'Ok-new'

--- heroku/src/main/java/com/ejemplo/heroku/controller/StatusController.java
+++ heroku/src/main/java/com/ejemplo/heroku/controller/StatusController.java
@@ -9,6 +9,6 @@ public class StatusController {
 
     @GetMapping("/status")
     public ResponseEntity<String> status() {
-        return ResponseEntity.ok("NotOk-new");
+        return ResponseEntity.ok("Ok-new");
     }
 }
